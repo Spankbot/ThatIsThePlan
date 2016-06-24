927ee0d9104db976b3565ec9de130c1fe3b6a3fb
Spankbot <nessisfading@gmail.com>
2016-06-24 14:31:05 -0700

    Code

---
+++ test.py
@@ -0,0 +1,23 @@
+from sys import exit
+import time
+
+
+
+def name():
+# Asks the user if he wants to change the name or not.
+ print "Do you want to change your name?"
+ option = raw_input("Yes/No:")
+ 
+ while option != "Yes" or "No":
+  if option == "Yes":
+   print "Write your name:"
+   name_char = raw_input("> ")
+   exit()
+  elif option == "No":
+   print "Alright,let's start then."
+   exit()
+  else:
+   print "Please enter Yes or No"
+   name()
+     
+name()
