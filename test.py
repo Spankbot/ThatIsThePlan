517727009b1de71daa192b023c31a6a1eb02657a
Spankbot <nessisfading@gmail.com>
2016-06-24 16:55:50 -0700

    Main code
    Fixed while loop and added a room.

--- test.py
+++ test.py
@@ -1,26 +1,48 @@
 from sys import exit
 import time
 
+#An introduction.
+print "\t\t\tTHAT IS THE PLAN \nLet's start with your name."
 
-
+#An easy way to tell where are you.
+def locationsign(location):
+ print "\n\t~~~---===(You are in %s)===---~~~\n" % location
+ 
+ 
+def start():
+#Starting room
+ locationsign("your home")
+ print '''
+ You are on your sofa,listening a weird music.
+ You are came exhausted from your work 
+ and the only thing you want to do is relax.
+ You hear your telephone ringing.
+ '''
+ prompt = raw_input("> ")
+ 
+ 
+ 
+ 
 def name():
 # Asks the user if he wants to change the name or not.
  print "Do you want to change your name?"
- option = raw_input("Yes/No:")
+ option = raw_input("Yes/No: ")
  
  option = option.lower()
  
- while option != "yes" or "no":
-  if option == "yes":
-   print "Write your name:"
-   name_char = raw_input("> ")
-   exit()
+ if option == "yes":
+  print "Write your name:"
+  name_char = raw_input("> ")
    
-  elif option == "no":
-   print "Alright,let's start then."
-   exit()
-  else:
+ elif option == "no":
+  print "Alright,let's start then."
+   
+ else:
+  while option != "yes" or "no":
    print "Please enter Yes or No"
    name()
      
 name()
+start()
+
+
