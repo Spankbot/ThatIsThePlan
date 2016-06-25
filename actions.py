8024697b72e6e51d5beb0f92f5a9409478dc8c7c
Spankbot <nessisfading@gmail.com>
2016-06-24 17:17:58 -0700

    Actions

---
+++ actions.py
@@ -0,0 +1,30 @@
+def sentence():
+ print "%s %s the %"
+
+def answer()
+ print "You answered t
+def cut()
+
+def drink()
+
+def go()
+
+def hit()
+
+def look()
+
+def respond()
+
+
+
+
+
+
+
+
+actions = [answer, cut, drink, go, hit, look, respond]
+actors = ["Valek", "Gwen"]
+subject =["You", "He" , "She", "Him", "Her", "They"]
+verb = ["answer", "cut", "drank", "went", "hit", "looked", "respond"]
+
+	
